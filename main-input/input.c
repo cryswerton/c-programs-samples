757510868eff03fb22e314afc39aac9643fae70a
cryswerton <cryswertons@gmail.com>
2021-06-26 20:29:46 -0300

    input on main function sample

---
+++ main-input/input.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+#include "cs50.h"
+
+// Usage: make file, ./file or ./file Crys
+
+int main(int argc, string argv[]){
+
+	if(argc == 2){
+		printf("Hello, %s!\n", argv[1]);
+	}else{
+		printf("Hello, world!\n");
+	}
+
+}
